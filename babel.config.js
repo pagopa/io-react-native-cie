a3edbe109add22d0212615552feff1396fad8bd9
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-12-02 13:03:14 +0100

    removes jest and babel misleading config

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  presets: [
-    'module:react-native-builder-bob/babel-preset',
-    ['@babel/preset-typescript', { allowDeclareFields: true }],
-  ],
-};
--- jest/react-native-mock.js
+++
@@ -1,2 +0,0 @@
-// Mock for react-native to avoid parsing issues
-module.exports = {};
